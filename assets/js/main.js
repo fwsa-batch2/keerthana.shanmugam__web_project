8d0a94f7149c0cd2074c95233620070c15ca3081
keerthana-shanmugam <keerthanas089@gmail.com>
2021-12-06 20:14:51 +0530

    added code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 function submitHandler(){
     alert('Your Username and Password send to Your email_id successfully');
     }
+
+ function mypassword(){
+     event.preventDefault();
+
+     let pass1 = document.getElementById("password").value;
+     let pass2 = document.getElementById("confirm password").value;
+    
+     if (pass1 == pass2){
+         console.log("password matched")
+         window.location.replace("login.html")
+     }
+     else {
+         console.log("password does not matched");
+         alert("password does not matched");
+     }
+ }
